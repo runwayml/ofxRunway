3b5b23d0192852a3982c9edf3efa0c34d0cd0eb7
Gene Kogan <kogan.gene@gmail.com>
2019-02-13 21:08:42 -0500

    bundle data structure

--- src/ofxRunway.cpp
+++ src/ofxRunway.cpp
@@ -1,61 +1,108 @@
 #include "ofxRunway.h"
 
 
-void ofxRunway::setup(string host) {
+void ofxRunway::setup(string host, string outputKey) {
     this->host = host;
+    this->outputKey = outputKey;
 }
 
 void ofxRunway::updateThread() {
 
-    ofPixels pixelsToReceive;
     ofPixels pixelsToSend;
-    
+
     ofxIO::Base64Encoding base64Encoder;
-    
-    while (isRunning() && input.receive(pixelsToReceive))
+
+
+    ofxRunwayBundle bundleToReceive;
+
+    while (isRunning() && input.receive(bundleToReceive))
     {
         if (input.size() > 1)
             continue;
-        
-        // Save the incoming pixels to a buffer using JPG compression.
-        ofBuffer compressedPixels;
-        ofSaveImage(pixelsToReceive, compressedPixels, OF_IMAGE_FORMAT_JPEG);
 
-        // Encode the compressed pixels in base64.
-        ofxIO::ByteBuffer base64CompressedPixelsIn;
-        base64Encoder.encode(ofxIO::ByteBuffer(compressedPixels.getData(), compressedPixels.size()),
-                             base64CompressedPixelsIn);
+        ofJson json = {};
 
-        // Setup the client request
-        multimap<string, string> formFields = {{ "data", base64CompressedPixelsIn.toString() }};
+        // iterate through bundle vectors
+        for (auto const &e1 : bundleToReceive.vectors) {
+          string key = e1.first;
+          vector<float> value = e1.second;
+          json[key] = value;
+        }
+
+        // iterate through bundle floats
+        for (auto const &e1 : bundleToReceive.floats) {
+          string key = e1.first;
+          float value = e1.second;
+          json[key] = value;
+        }
+
+        // iterate through bundle integers
+        for (auto const &e1 : bundleToReceive.ints) {
+          string key = e1.first;
+          int value = e1.second;
+          json[key] = value;
+        }
+
+        // iterate through bundle strings
+        for (auto const &e1 : bundleToReceive.strings) {
+          string key = e1.first;
+          string value = e1.second;
+          json[key] = value;
+        }
+
+        // iterate through bundle images
+        for (auto const &e1 : bundleToReceive.images) {
+          string key = e1.first;
+          ofPixels pixelsToReceive = e1.second;
+
+          // Save the incoming pixels to a buffer using JPG compression.
+          ofBuffer compressedPixels;
+          ofSaveImage(pixelsToReceive, compressedPixels, OF_IMAGE_FORMAT_PNG);
+
+          // Encode the compressed pixels in base64.
+          ofxIO::ByteBuffer base64CompressedPixelsIn;
+          base64Encoder.encode(ofxIO::ByteBuffer(compressedPixels.getData(), compressedPixels.size()),
+                               base64CompressedPixelsIn);
+
+          json[key] = base64CompressedPixelsIn.toString()+"==";
+        }
+
+        // setup json request
         ofxHTTP::Client client;
-        ofxHTTP::PostRequest request(host);
-        request.addFormFields(formFields);
-        
+        ofxHTTP::JSONRequest request(host);
+        request.setJSON(json);
+
         // Send them to Runway server
         try
         {
             ofLog() << "execute request " << ++numRequests;
             auto response = client.execute(request);
-            
+            ofLog() << " did request " << endl;
+
             if (response->getStatus() == Poco::Net::HTTPResponse::HTTP_OK)
             {
-                // Decode base64.
-                ofxIO::ByteBuffer base64CompressedPixelsOut;
-                ofxIO::ByteBuffer base64CompressedPixelsProcessed(response->buffer());
-                base64Encoder.decode(base64CompressedPixelsProcessed,
-                                     base64CompressedPixelsOut);
-                
-                // Decompress JPG
-                ofLoadImage(pixelsToSend, ofBuffer(base64CompressedPixelsOut.getCharPtr(),
-                                                   base64CompressedPixelsOut.size()));
+              ofLogNotice("ofApp::setup") << "Response success, expecting " << response->estimatedContentLength() << " bytes.";
 
-                // Send the pixels back to the output channel.
-                output.send(pixelsToSend);
-            }
-            else
-            {
-                ofLogError("ofApp::setup") << response->getStatus() << " " << response->getReason();
+              // Buffer the response, or otherwise consume the stream.
+              ofJson responseJson = response->json();
+
+              //ofLogNotice("ofApp::setup") << "Content Begin";
+              //std::cout << responseJson.dump(4) << std::endl;
+              //ofLogNotice("ofApp::setup") << "Content End";
+
+              // extract image
+              string imageB64 = responseJson[outputKey].dump();
+              imageB64 = imageB64.substr(1, imageB64.size()-2);//+"==";;
+              imageB64 = imageB64.substr(imageB64.find(",") + 1);
+
+              ofBuffer decodedBuffer;
+              decodedBuffer.set(ofxIO::Base64Encoding::decode(imageB64));
+
+              // Decompress JPG
+              ofLoadImage(pixelsToSend, decodedBuffer);
+
+              // Send the pixels back to the output channel.
+              output.send(pixelsToSend);
             }
         }
         catch (const Poco::Exception& exc)
@@ -66,6 +113,6 @@ void ofxRunway::updateThread() {
         {
             ofLogError("ofApp::setup") << exc.what();
         }
+
     }
 }
-
--- src/ofxRunway.h
+++ src/ofxRunway.h
@@ -4,29 +4,39 @@
 #include "ofxHTTP.h"
 #include "ofxIO.h"
 
+
+struct ofxRunwayBundle {
+  map<string, ofPixels> images;
+  map<string, float> floats;
+  map<string, int> ints;
+  map<string, string> strings;
+  map<string, vector<float>> vectors;
+};
+
 class ofxRunway: public ofxIO::Thread {
 public:
     ofxRunway(): ofxIO::Thread(std::bind(&ofxRunway::updateThread, this)) {}
     virtual ~ofxRunway(){}
 
-    void setup(string host);
+    void setup(string host, string outputKey);
 
-    void send(ofPixels & pixels) {
-        input.send(pixels);
+    void send(ofxRunwayBundle & bundle) {
+        input.send(bundle);
     }
+
     bool tryReceive(ofPixels & pixels) {
         return output.tryReceive(pixels);
     }
-    
+
 protected:
-    
+
     void updateThread();
-    
-    ofxIO::ThreadChannel<ofPixels> input;
+
+    ofxIO::ThreadChannel<ofxRunwayBundle> input;
     ofxIO::ThreadChannel<ofPixels> output;
 
     string host;
-    
-    
+    string outputKey;
+
     int numRequests = 0;
 };
